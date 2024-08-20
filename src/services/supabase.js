8c7dc730564ab3915f46f9af0df6f9942bec17a3
Bryan Tjandra <tjandra.benedict@gmail.com>
2024-08-20 16:32:21 +0700

    setup supabase db and client

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://nsaqteixgvnlvfaaqagp.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zYXF0ZWl4Z3ZubHZmYWFxYWdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQxNDE0MjIsImV4cCI6MjAzOTcxNzQyMn0.OaG_9b0ELScbrkBQAl5fPr5w3JLFMQzMjGYPARDLHKM";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
