2865d25db3384c30a9cdfcd042031564cfcf1de3
Bryan Tjandra <tjandra.benedict@gmail.com>
2024-08-26 12:44:38 +0800

    setup react hook form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,10 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getCabins } from "../services/apiCabins";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import Button from "../ui/Button";
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -15,6 +18,14 @@ function Cabins() {
       <Row>
         <CabinTable />
       </Row>
+      <Button
+        onClick={() => {
+          setShowForm(!showForm);
+        }}
+      >
+        Add new cabin
+      </Button>
+      {showForm && <CreateCabinForm />}
     </>
   );
 }
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -22,6 +22,7 @@ const Container = styled.div`
   flex-direction: column;
   gap: 3.2rem;
 `;
+
 function AppLayout() {
   return (
     <StyledAppLayout>
