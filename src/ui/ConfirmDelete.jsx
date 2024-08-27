c3fd9982da6fe2f23027d5d63f1be540ce971cbb
Bryan Tjandra <tjandra.benedict@gmail.com>
2024-08-27 23:49:06 +0800

    confirm cabin deletions

--- src/ui/ConfirmDelete.jsx
+++ src/ui/ConfirmDelete.jsx
@@ -20,7 +20,13 @@ const StyledConfirmDelete = styled.div`
   }
 `;
 
-function ConfirmDelete({ resourceName, onConfirm, disabled }) {
+function ConfirmDelete({
+  resourceName,
+  onConfirm,
+  disabled,
+  onConfirm,
+  onCloseModal,
+}) {
   return (
     <StyledConfirmDelete>
       <Heading as="h3">Delete {resourceName}</Heading>
@@ -30,10 +36,14 @@ function ConfirmDelete({ resourceName, onConfirm, disabled }) {
       </p>
 
       <div>
-        <Button variation="secondary" disabled={disabled}>
+        <Button
+          variation="secondary"
+          disabled={disabled}
+          onClick={onCloseModal}
+        >
           Cancel
         </Button>
-        <Button variation="danger" disabled={disabled}>
+        <Button variation="danger" disabled={disabled} onClick={onConfirm}>
           Delete
         </Button>
       </div>
