289f0384d7369dbc664028816e46453cf7d6db41
Bryan Tjandra <tjandra.benedict@gmail.com>
2024-08-30 09:35:18 +0800

    implement pagination for booking details

---
+++ src/features/bookings/useBooking.js
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams, useSearchParams } from "react-router-dom";
+import { getBooking } from "../../services/apiBookings";
+
+export function useBooking() {
+  const { bookingId } = useParams();
+
+  const {
+    isLoading,
+    data: booking,
+    error,
+  } = useQuery({
+    queryKey: ["booking", bookingId],
+    queryFn: () => getBooking(bookingId),
+  });
+  return { isLoading, booking, error };
+}
