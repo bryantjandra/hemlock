2ce80927c4179a79802524d7f2418238a8395bcd
Bryan Tjandra <tjandra.benedict@gmail.com>
2024-09-01 10:31:58 +0800

    add dark mode feature

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import ButtonIcon from "./ButtonIcon";
 import { HiOutlineUser } from "react-icons/hi2";
 import Logout from "../features/authentication/Logout";
+import DarkModeToggle from "./DarkModeToggle";
 const StyledHeaderMenu = styled.ul`
   display: flex;
   gap: 0.4rem;
@@ -19,6 +20,9 @@ function HeaderMenu() {
         >
           <HiOutlineUser />
         </ButtonIcon>
+      </li>
+      <li>
+        <DarkModeToggle />
       </li>
       <li>
         <Logout />
